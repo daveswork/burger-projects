978d3b6fc7513bddad3347bc813683b655ec4b5d
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-24 13:47:31 -0400

    added more stuff

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ fetch("http://localhost:3000/burgers")
 function addBurgerToBurgerList(burger){
     const listElement = document.createElement("li")
 
+    const idElement = document.createElement("h3")
+    idElement.textContent = burger.id
+
     const nameElement = document.createElement('h3')
     nameElement.textContent = `Name: ${burger.name}`
 
-    const idElement = document.createElement("h3")
+    const imageElement = document.createElement("img")
+    imageElement.src = burger.image
 
+    const priceElment = document.createElement("h3")
+    priceElment.textContent = burger.price
+
+    listElement.appendChild(idElement)
     listElement.appendChild(nameElement)
+    listElement.appendChild(imageElement)
+    listElement.appendChild(priceElment)
+    
     console.log(listElement)
     burgersList.appendChild(listElement)
 }
