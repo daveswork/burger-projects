ce6c9ea828d9d03361132375311243fdbbecd3e7
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-24 13:32:35 -0400

    lots of stuff

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+const burgersList = document.getElementById('burgers-list')
+
+console.log(burgersList)
 
 fetch("http://localhost:3000/burgers")
 .then(response => response.json())
 .then(burgers => {
     console.log(burgers)
+    burgers.forEach(addBurgerToBurgerList)    
 })
+
+function addBurgerToBurgerList(burger){
+    const listElement = document.createElement("li")
+
+    const nameElement = document.createElement('h3')
+    nameElement.textContent = `Name: ${burger.name}`
+
+    const idElement = document.createElement("h3")
+
+    listElement.appendChild(nameElement)
+    console.log(listElement)
+    burgersList.appendChild(listElement)
+}
